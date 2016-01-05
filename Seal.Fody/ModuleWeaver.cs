1ffc82d06790a2d0f95c0edd87ae40a6bc5db902
Kamil Mrzygłód <kmrzyglod@moneygram.com>
2016-01-05 15:01:43 +0100

    Weaver functionality

---
+++ Seal.Fody/ModuleWeaver.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using Mono.Cecil;
+
+namespace Seal.Fody
+{
+    public class ModuleWeaver
+    {
+        public ModuleDefinition ModuleDefinition { get; set; }
+
+        public void Execute()
+        {
+            foreach (var type in ModuleDefinition.Types.Where(type => type.IsAbstract == false && type.IsSealed == false))
+            {
+                type.IsSealed = true;
+            }
+        }
+    }
+}
